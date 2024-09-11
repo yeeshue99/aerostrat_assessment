3ab5dca0cb5bd0ca1221d3a09c8e3f108bf48fae
yeeshue99 <35947745+yeeshue99@users.noreply.github.com>
2024-09-11 09:28:39 -0700

    Initial Commit

---
+++ dad-joke-supplier/src/app/j/[jokeId]/loading.tsx
@@ -0,0 +1,6 @@
+import CircularSpinner from "../../../../components/util/Spinner"
+
+export default function Loading() {
+    // You can add any UI inside Loading, including a Skeleton.
+    return <CircularSpinner />
+  }
---
+++ dad-joke-supplier/src/app/layout.tsx
@@ -0,0 +1,47 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+import SearchAppBar from "../../components/Header/SearchAppBar";
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
+import { ThemeProvider } from '@mui/material/styles';
+import RandomJokeButton from "../../components/util/RandomJokeButton";
+import theme from '../theme';
+import { Box } from "@mui/system";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Aerostrat Dad Joke Generator",
+  description: "Dad joke generator",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>
+            <SearchAppBar />
+            <Box sx={{width: "80%"}}>
+              {children}
+            <RandomJokeButton />
+            </Box>
+          </ThemeProvider>
+        </AppRouterCacheProvider>
+      </body>
+    </html>
+  );
+}
