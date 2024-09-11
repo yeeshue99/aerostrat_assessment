58defe413f315aa5d44cd4933168c3b9f93b5585
yeeshue99 <35947745+yeeshue99@users.noreply.github.com>
2024-09-11 10:16:09 -0700

    migrate to netlify

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    /**
-     * Set base path. This is the slug of your GitHub repository.
-     *
-     * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-     */
-    basePath: "/aerostrat_assessment",
-
-    /**
-     * Disable server-based image optimization. Next.js does not support
-     * dynamic features with static exports.
-     *
-     * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
-     */
-    images: {
-        unoptimized: true,
-    },
-};
+const nextConfig = {};
 
 export default nextConfig;
